42e5b5c4fd765dce2e5866d363f02f3eb26905c0
ccmaloney <charles.c.maloney@gmail.com>
2025-03-12 16:59:08 -0500

    wipe clean

--- src/types.rs
+++ src/types.rs
@@ -1,9 +1,1 @@
-use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
-#[serde(rename_all = "snake_case")]
-pub enum OrthogonalizationMethod {
-    GramSchmidt,
-    Pca,
-    Regression,
-}
